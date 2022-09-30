c1b3ae700bab56797100bcc22fc5894ca0bfb6a9
Chandu-Vanam <vnmchandu@gmail.com>
2022-09-30 12:58:45 +0530

    final total

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -11,26 +11,30 @@ const CartItem = (props) => {
   return (
     <div className="cart-item">
       <div className="left-block">
-        <img style={styles.image} />
+        <img style={styles.image} src={product.img} />
       </div>
       <div className="right-block">
-        <div style={{ fontSize: 25 }}>{title}</div>
-        <div style={{ color: '#777' }}>Rs {price} </div>
-        <div style={{ color: '#777' }}>Qty: {qty} </div>
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Rs {price} </div>
+        <div style={ { color: '#777' } }>Qty: {qty} </div>
         <div className="cart-item-actions">
           {/* Buttons */}
           <img
             alt="increase"
             className="action-icons"
-            src="https://tse3.mm.bing.net/th?id=OIP.Es5ynla8quztUh7pQ9grAAHaHa&pid=Api&P=0"
+            src='http://cdn.onlinewebfonts.com/svg/img_28497.png'
             onClick={() => onIncreaseQuantity(product)}
           />
-          <img alt="decrease"
+          <img
+            alt="decrease"
             className="action-icons"
-            src="https://tse1.mm.bing.net/th?id=OIP.8Vh_442i30VigdcNjYJr4QHaHY&pid=Api&P=0"
+            src="https://tse2.mm.bing.net/th?id=OIP.dnYgr6NUp1K4VyFjEVTeHgHaHa&pid=Api&P=0"
             onClick={() => onDecreaseQuantity(product)}
           />
-          <img alt="delete" className="action-icons" src="https://tse1.mm.bing.net/th?id=OIP.BXpgGsCGLNi2cdVeW7o6zQHaIp&pid=Api&P=0"
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://cdn.onlinewebfonts.com/svg/img_252944.png"
             onClick={() => onDeleteProduct(product.id)}
           />
         </div>
